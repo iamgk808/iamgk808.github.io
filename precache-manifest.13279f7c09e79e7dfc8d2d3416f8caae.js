34495a2191ec5c7ae82e25ef0515bbdfb0e45ecf
gk808 <gk808.root@gmail.com>
2021-09-22 14:41:32 +0530

    website

---
+++ precache-manifest.13279f7c09e79e7dfc8d2d3416f8caae.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e300ca42e5ccbe8ada8067021a4ebf76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca3b53c534baa090c53c",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "3aa45f692e94f9baef42",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "126d598f69282be5a436",
+    "url": "/static/css/main.ad8bc99d.chunk.css"
+  },
+  {
+    "revision": "1580aae22528b3c9e824",
+    "url": "/static/js/2.8e2a1906.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.8e2a1906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3b53c534baa090c53c",
+    "url": "/static/js/3.b088ca38.chunk.js"
+  },
+  {
+    "revision": "3aa45f692e94f9baef42",
+    "url": "/static/js/4.2580accd.chunk.js"
+  },
+  {
+    "revision": "126d598f69282be5a436",
+    "url": "/static/js/main.1477b2a6.chunk.js"
+  },
+  {
+    "revision": "0c5fecf5f7880d11f5f7",
+    "url": "/static/js/runtime-main.9a6b0f2e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "40ffd7a06ec22d21bd0e6545a109773e",
+    "url": "/static/media/deskera.40ffd7a0.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "5f7cc0541d1c3e667fa14a5975fc5d31",
+    "url": "/static/media/goodrx.5f7cc054.bin"
+  },
+  {
+    "revision": "73ef27943eb823cdb6562ad211689130",
+    "url": "/static/media/govtech.73ef2794.bin"
+  },
+  {
+    "revision": "5e8c44db4a9a1e0222be605b2d9590b5",
+    "url": "/static/media/mailru.5e8c44db.bin"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "71340f201c70024a87aede46fe00d737",
+    "url": "/static/media/messagebird.71340f20.bin"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "38c43d90ff7ade4ddf4ba15d540a7256",
+    "url": "/static/media/thm_certt.38c43d90.png"
+  },
+  {
+    "revision": "dd0ddd2f38a78b6acb93e969d0deb2b8",
+    "url": "/static/media/thm_rank.dd0ddd2f.png"
+  },
+  {
+    "revision": "53f9ebf6859e4a13368814952c2f276c",
+    "url": "/static/media/usmil.53f9ebf6.bin"
+  }
+]);
